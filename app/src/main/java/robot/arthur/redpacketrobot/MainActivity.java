15893b4c7b598987017aa0f2cc3f3e090acb19be
changliwei1993 <changliwei1993@gmail.com>
2016-01-27 15:00:56 +0800

    add two buttons,and add a monitor service

--- app/src/main/java/robot/arthur/redpacketrobot/MainActivity.java
+++ app/src/main/java/robot/arthur/redpacketrobot/MainActivity.java
@@ -1,13 +1,39 @@
 package robot.arthur.redpacketrobot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+    private Button start_monitor,stop_monitor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+    private void initView(){
+        start_monitor=(Button)findViewById(R.id.start_monitor);
+        stop_monitor=(Button)findViewById(R.id.stop_monitor);
+        start_monitor.setOnClickListener(this);
+        stop_monitor.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.start_monitor:
+                break;
+            case R.id.stop_monitor:
+                break;
+            default:
+                break;
+        }
+
     }
 }
